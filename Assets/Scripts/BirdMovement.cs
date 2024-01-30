9103a2f18448b2fbe0892ed3cea5b533e56eda91
Viper <41963246+SkullViper7@users.noreply.github.com>
2024-01-30 22:11:39 +0100

    rotation

--- Assets/Scripts/BirdMovement.cs
+++ Assets/Scripts/BirdMovement.cs
@@ -8,6 +8,9 @@ public class BirdMovement : MonoBehaviour
     [SerializeField]
     float _jumpForce;
 
+    [SerializeField]
+    float _rotationSpeed;
+
     Rigidbody rb;
 
     private void Start()
@@ -15,8 +18,14 @@ public class BirdMovement : MonoBehaviour
         rb = GetComponent<Rigidbody>();
     }
 
+    private void FixedUpdate()
+    {
+        transform.rotation = Quaternion.Euler(0, 180, -rb.velocity.y * _rotationSpeed);
+    }
+
     public void OnJump()
     {
+        rb.useGravity = true;
         rb.velocity = Vector3.zero;
         rb.AddForce(Vector3.up * _jumpForce, ForceMode.Impulse);
     }
